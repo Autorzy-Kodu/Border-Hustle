9bf9797e890ac32c0222b1c38591cfb5b5bf796b
Kimel-PK <57668948+Kimel-PK@users.noreply.github.com>
2023-04-14 18:46:21 +0200

    sprites

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -2,13 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIManager : Singleton<UIManager>
 {
 	private TextMeshProUGUI cashText;
+	private Slider heatSlider;
 	
 	public float Cash
 	{
 		set => cashText.text = $"${value}";
+	}
+
+	public float Heat
+	{
+		set => heatSlider.value = value;
 	}
 }
